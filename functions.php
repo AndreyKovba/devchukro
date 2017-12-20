37e591b756ff5544bdbd31de4ed633a13397fb0c
AndreyKovba <megadronblch@googlemail.com>
2017-12-20 13:41:38 +0500

    added buy button

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function my_theme_enqueue_styles() {
     $parent_style = 'parent-style';
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css?v=1312171' );
     wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css?v=151217',
+        get_stylesheet_directory_uri() . '/style.css?v=201217',
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
@@ -16,6 +16,61 @@ function child_theme_translation() {
 }
 add_action( 'after_setup_theme', 'child_theme_translation' );
 
+add_filter('woocommerce_add_to_cart_redirect', 'themeprefix_add_to_cart_redirect');
+function themeprefix_add_to_cart_redirect() {
+    if(!isset($_GET['buy'])) {
+        return;
+    }
+    global $woocommerce;
+    $checkout_url = wc_get_checkout_url();
+    return $checkout_url;
+}
+
+add_action( 'woocommerce_after_shop_loop_item', 'avada_woocommerce_buy_button', 10 );
+
+function avada_woocommerce_buy_button( $args = array() ) {
+    global $product;
+
+    if ( $product &&
+        ( ( $product->is_purchasable() && $product->is_in_stock() ) || $product->is_type( 'external' ) )
+    ) {
+        $defaults = array(
+            'quantity' => 1,
+            'class'    => implode( ' ', array_filter( array(
+                'button',
+                'product_type_' . $product->product_type,
+                $product->is_purchasable() && $product->is_in_stock() ? 'add_to_cart_button' : '',
+                $product->supports( 'ajax_add_to_cart' ) ? 'ajax_add_to_cart' : ''
+            ) ) )
+        );
+        $args = apply_filters( 'woocommerce_loop_add_to_cart_args', wp_parse_args( $args, $defaults ), $product );
+        ?>
+        <div class="clearfix">
+            <?php drawBuyButton($args); ?>
+        </div>
+        <?php
+    }
+}
+
+function drawBuyButton($args){
+    global $product;
+    $href = $product->add_to_cart_url();
+    $uriArray = explode('/', $href);
+    if(strpos($uriArray[count($uriArray) - 1], '?') === false ){
+        $href .= '?';
+    }
+    $href .= '&buy=1';
+    echo apply_filters( 'woocommerce_loop_add_to_cart_link',
+        sprintf( '<a rel="nofollow" href="%s" data-quantity="%s" data-product_id="%s" data-product_sku="%s" class="button-left %s">%s</a>',
+            esc_url( $href ),
+            esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
+            esc_attr( $product->get_id() ),
+            esc_attr( $product->get_sku() ),
+            esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
+            esc_html( 'Köpa' )
+        ),
+        $product );
+}
 
 add_action( 'avada_after_main_container','avada_child_after_main_container', 10 );
 function avada_child_after_main_container() {
