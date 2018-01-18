ac03368aa6b23602ee6cccfddace728da093cf65
AndreyKovba <megadronblch@googlemail.com>
2018-01-18 12:23:06 +0500

    filtering margin reduced

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function my_theme_enqueue_styles() {
     $parent_style = 'parent-style';
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css?v=150118' );
     wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css?v=1801182',
+        get_stylesheet_directory_uri() . '/style.css?v=1801183',
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
