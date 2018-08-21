480073ecdd2e3f4b27ac172eb7dc70331bc1f8e7
AndreyKovba <megadronblch@googlemail.com>
2018-08-21 15:16:50 +0500

    menu now sticky

--- functions.php
+++ functions.php
@@ -2,9 +2,9 @@
 
 function my_theme_enqueue_styles() {
     $parent_style = 'parent-style';
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css?v=1708181' );
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css?v=210818' );
     wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/main.css?v=1708181',
+        get_stylesheet_directory_uri() . '/main.css?v=210818',
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
