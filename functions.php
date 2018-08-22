de26c1a59392283719a009d02ba38aca2f0c99e0
AndreyKovba <megadronblch@googlemail.com>
2018-08-22 14:34:05 +0500

    z-index fixed for ie

--- functions.php
+++ functions.php
@@ -2,9 +2,9 @@
 
 function my_theme_enqueue_styles() {
     $parent_style = 'parent-style';
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css?v=2208181' );
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css?v=2208182' );
     wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/main.css?v=2208181',
+        get_stylesheet_directory_uri() . '/main.css?v=2208182',
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
