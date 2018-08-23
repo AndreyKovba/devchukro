fdc0679fb1cf63f63edae7e4a5412a65d2bbfa5c
AndreyKovba <megadronblch@googlemail.com>
2018-08-23 17:34:16 +0500

    header min-heigh fixed

--- functions.php
+++ functions.php
@@ -2,9 +2,9 @@
 
 function my_theme_enqueue_styles() {
     $parent_style = 'parent-style';
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css?v=2208185' );
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css?v=230818' );
     wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/main.css?v=2208185',
+        get_stylesheet_directory_uri() . '/main.css?v=230818',
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
